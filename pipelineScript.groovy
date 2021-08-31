788c9ce43545b83ecda7ed114201bbd157988ada
ravi3000 <sraavee@gmail.com>
2021-08-31 23:45:20 +0530

    further comment

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -14,7 +14,7 @@ agent any
                 {                   
 		            //bat 'nuget restore SolutionName.sln'
                     //bat "\"${tool 'MSBuild'}\" ConsoleApp1\ConsoleApp1.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
-                    bat dotnet msbuild ConsoleApp1\\ConsoleApp1.sln /p:Configuration=Release /p:Platform="Any CPU"
+                    bat dotnet msbuild ConsoleApp1git\ConsoleApp1.sln /p:Configuration=Release /p:Platform="Any CPU"
                 }
            }
         // stage ('Archive')
