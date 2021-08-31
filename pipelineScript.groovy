c5119f60394f7e8fd839c7b029ee6526e20fe288
ravi3000 <sraavee@gmail.com>
2021-09-01 00:14:07 +0530

    added dotnet restore[F

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -12,7 +12,8 @@ agent any
            {
                 steps
                 {                   
-		            bat 'd:/Tools/nuget.exe restore ConsoleApp1/ConsoleApp1.sln'
+		            //bat 'd:/Tools/nuget.exe restore ConsoleApp1/ConsoleApp1.sln'
+		            bat 'dotnet restore ConsoleApp1/ConsoleApp1.sln'
                     //bat "\"${tool 'MSBuild'}\" ConsoleApp1\ConsoleApp1.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
                     bat 'dotnet msbuild ConsoleApp1/ConsoleApp1.sln /p:Configuration=Release /p:Platform="Any CPU"'
                 }
