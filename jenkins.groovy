2e0ee478f41fc9bd9baa19869008ebd62b878856
ravi3000 <sraavee@gmail.com>
2021-08-31 22:56:06 +0530

    real issue fixed

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,19 +1,12 @@
-node
-{
-  stage ‘SCM’
-  {
-  echo “Gathering code form version control”
-  }
-  stage ‘BUILD’
-  {
-    echo “Building”
-  }
-  stage ‘TEST’
-  {
-    echo “Testing”
-  }
-  stage ‘DEPLOY’
-  {
-    echo "Deploying"
-  }
+pipeline{
+agent any{
+stages{
+stage('CheckOut'){
+steps{
+  'Hello World'
 }
+}
+}
+}
+}
+
