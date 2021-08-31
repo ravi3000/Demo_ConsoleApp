183c89e7ef658b6021f9b2c338705109f69436a0
ravi3000 <sraavee@gmail.com>
2021-08-31 23:25:01 +0530

    added further stages

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -1,10 +1,29 @@
 pipeline {
 agent any
     stages {
-        stage('CheckOut') {
+        stage('CheckOut')
+         {
             steps {
                 echo 'Hello World'
             }
         }
+        stage('Build')
+           {
+                steps
+                {                   
+		            //bat 'nuget restore SolutionName.sln'
+		            bat "\"${tool 'MSBuild'}\" ConsoleApp1.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+                }
+           }
+        stage ('Archive')
+        {
+            steps
+            {                   
+		         bat archive 'ProjectName/bin/Release/**'
+            }
+        }
+
+        }
     }
-}
+
+
