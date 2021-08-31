5f3846e76fff612ae7e696503582e75325e888a5
ravi3000 <sraavee@gmail.com>
2021-08-31 22:48:23 +0530

    fixed syntax error

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,14 +1,19 @@
-node{
-stage(‘SCM’){
- echo “Gathering code form version control”
- }
-stage(‘BUILD’){
-echo “Building”
- }
-stage(‘TEST’){
-echo “Testing….”
- }
-stage(‘DEPLOY’){
-   echo ‘Deploying….’
- }
+node
+{
+  stage(‘SCM’)
+  {
+  echo “Gathering code form version control”
+  }
+  stage(‘BUILD’)
+  {
+    echo “Building”
+  }
+  stage(‘TEST’)
+  {
+    echo “Testing”
+  }
+  stage(‘DEPLOY’)
+  {
+    echo "Deploying"
+  }
 }
